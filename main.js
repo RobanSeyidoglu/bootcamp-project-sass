22fa3113e2d8a4d70e8ceb30e934d9f3936e264c
RobanSeyidoglu <robanseyidoglu@gmail.com>
2023-10-02 22:44:52 +0300

    Update Js code to make transition effect on ul menu and burger icon

--- main.js
+++ main.js
@@ -4,10 +4,16 @@ let menu = document.querySelector("ul");
 let body = document.body;
 if (getComputedStyle(toggleMenu).display !== "none") {
   toggleMenu.onclick = function () {
-    if (getComputedStyle(menu).display === "flex") {
-      menu.style.display = "none";
+    if (menu.classList.contains("ul-hide")) {
+      toggleMenu.style.transition = "all 0.2s";
+      toggleMenu.style.outlineWidth = "3px";
+      menu.classList.remove("ul-hide");
+      menu.classList.add("ul-show");
     } else {
-      menu.style.display = "flex";
+      menu.classList.add("ul-hide");
+      menu.classList.remove("ul-show");
+      toggleMenu.style.transition = "all 0.2s";
+      toggleMenu.style.outlineWidth = "1px";
     }
   };
   document.addEventListener("click", function (event) {
@@ -18,46 +24,48 @@ if (getComputedStyle(toggleMenu).display !== "none") {
       !(getComputedStyle(toggleMenu).display === "none")
     ) {
       console.log(toggleMenu.style.display);
-      menu.style.display = "none";
+      menu.classList.add("ul-hide");
+      menu.classList.remove("ul-show");
+      toggleMenu.style.transition = "all 0.2s";
+      toggleMenu.style.outlineWidth = "1px";
     }
   });
 } else if (toggleMenu.style.display === "none") {
-  menu.style.display = "flex";
+  menu.classList.remove("ul-hide");
+  menu.classList.add("ul-show");
+  toggleMenu.style.transition = "all 0.2s";
+  toggleMenu.style.outlineWidth = "3px";
 }
 document.addEventListener("DOMContentLoaded", function () {
   const links = document.querySelectorAll("ul li a");
 
-  // Add a click event listener to each link
   links.forEach(function (link) {
     link.addEventListener("click", function (event) {
-      // Prevent the default behavior of the link
       event.preventDefault();
 
-      // Change the text color to white for the clicked link
       link.style.color = "white";
 
-      // Revert the text color to its original color for all other links
       links.forEach(function (otherLink) {
         if (otherLink !== link) {
-          otherLink.style.color = ""; // Empty string to revert to default color
+          otherLink.style.color = "";
         }
       });
     });
   });
 });
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-      e.preventDefault();
-      
-      const targetId = this.getAttribute('href').substring(1);
-      const targetElement = document.getElementById(targetId);
-      
-      if (targetElement) {
-        targetElement.scrollIntoView({
-          behavior: 'smooth'
-        });
-      }
-    });
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute("href").substring(1);
+    const targetElement = document.getElementById(targetId);
+
+    if (targetElement) {
+      targetElement.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
   });
-  
+});
+
 // End Nav
